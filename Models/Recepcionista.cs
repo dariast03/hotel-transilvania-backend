c0ea40db9ca4bc12e02b287cc7d5a522bb9b1b08
AlexanderRG <alexander.rgareca@gmail.com>
2024-03-31 01:39:17 -0400

    RecepcionistaModel Added

--- Models/Recepcionista.cs
+++ Models/Recepcionista.cs
@@ -1,10 +1,19 @@
-﻿namespace HotelTransilvania.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelTransilvania.Models
 {
     public class Recepcionista
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Error, se requiere Cargo.")]
         public string Cargo { get; set; }
-        public string Description { get; set; }
-        public int Amount { get; set; }
+        public int IdPersona { get; set; }
+        [ForeignKey(nameof(IdPersona))]
+        public Persona Persona { get; set; }
+        public int IdUsuario { get; set; }
+        [ForeignKey(nameof(IdUsuario))]
+        public Usuario Usuario { get; set; }
     }
 }
