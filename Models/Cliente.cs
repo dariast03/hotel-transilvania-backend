80b385c7e7e0d1f6cbb50b91e7e75f9fdbdc8170
Dante Arias Tarifa <danteariastarifa@gmail.com>
2024-03-31 19:19:58 -0400

    Add MinLengths in Users Models

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,15 +7,22 @@ namespace HotelTransilvania.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Error, se requiere Nacionalidad.")]
-        public string Nacionalidad { get; set; }
+        [MinLength(2, ErrorMessage = "La longitud mínima de la Nacionalidad debe ser de dos caracteres.")]
+        public string Nacionalidad { get; set; } = null!;
+
         public string? Frecuencia { get; set; }
+
         public int IdPersona { get; set; }
+
         [ForeignKey(nameof(IdPersona))]
-        public Persona Persona { get; set; }
+        public Persona Persona { get; set; } = null!;
+
         public int IdUsuario { get; set; }
+
         [ForeignKey(nameof(IdUsuario))]
-        public Usuario Usuario { get; set; }
+        public Usuario Usuario { get; set; } = null!;
     }
 }
 
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,17 +2,27 @@
 
 namespace HotelTransilvania.Models
 {
+
     public class Usuario
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Error, se requiere Correo.")]
-        public string Correo { get; set; }
+        [MinLength(6, ErrorMessage = "El correo electrónico debe tener al menos 6 caracteres.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Correo { get; set; } = null!;
+
         [Required(ErrorMessage = "Error, se requiere Contraseña.")]
-        public string Contraseña { get; set; }
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; } = null!;
+
         [Required(ErrorMessage = "Error, se requiere Rol.")]
-        public string Rol { get; set; }
+        public string Rol { get; set; } = null!;
+
         [Required(ErrorMessage = "Error, se requiere Estado.")]
-        public string Estado { get; set; }
+        [MinLength(6, ErrorMessage = "El estado debe tener al menos 6 caracteres.")]
+        public string Estado { get; set; } = "ACTIVO";
     }
 }
