803891ade5c7fa88b529433231b5e7897e5a14fd
mauri123gutierrez <user.mauri123gutierrez@gmail.com>
2024-04-02 00:37:14 -0400

    update data base

--- Migrations/20240402043155_promocion.Designer.cs
+++ Migrations/20240402043155_promocion.Designer.cs
@@ -4,6 +4,7 @@ using HotelTransilvania.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelTransilvania.Migrations
 {
     [DbContext(typeof(RegisterLoginContext))]
-    partial class RegisterLoginContextModelSnapshot : ModelSnapshot
+    [Migration("20240402043155_promocion")]
+    partial class promocion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
